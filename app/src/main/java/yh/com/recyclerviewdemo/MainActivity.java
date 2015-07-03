d943d8a9f1ffbc36038fd7003b3a7d16f504a61b
yanhaogoogle <yanhao270894002@gmail.com>
2015-07-03 16:55:41 +0800

    Initial commit

---
+++ app/src/main/java/yh/com/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package yh.com.recyclerviewdemo;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
